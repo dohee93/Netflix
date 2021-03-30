a946a94dfb3572e79ea1abb98bea7b01181b4295
dohee93 <rlaehgml03@gmail.com>
2021-03-30 17:31:48 +0900

    Add changing hero background images

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+var imgNumPrev = 0;
+
+function changeBackground() {
+   var imgNum;
+   while(1) {
+      imgNum = Math.floor(Math.random() * 10);
+      if(imgNum != imgNumPrev) break;
+   } 
+   imgNumPrev = imgNum;
+   
+   document.getElementsByClassName('hero')[0].style = "background-image: url(img/poster_" + imgNum + ".jpg)";
+   document.getElementsByClassName('hero')[0].classList.remove('effect-fade-in');
+   void document.getElementsByClassName('hero')[0].offsetWidth;
+   document.getElementsByClassName('hero')[0].classList.add('effect-fade-in');
+}
+
+var changeHero = setInterval(changeBackground, 3000);
+
+
