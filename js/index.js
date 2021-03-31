d96694006e6c2f2889e082c921e50f54f3b0e2fa
dohee93 <rlaehgml03@gmail.com>
2021-03-31 20:33:48 +0900

    add faq section

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-
+// Hero Background JS
 var imgNumPrev = 0;
 
 function changeBackground() {
@@ -17,4 +17,34 @@ function changeBackground() {
 
 var changeHero = setInterval(changeBackground, 3000);
 
+
+
+
+
+// Accordion Toggle JS
+//var openState = [false,false,false,false,false,false];
+var openState = new Boolean(false);
+   
+   
+function contentOpen(target) {
+
+   for(i = 0; i < 6; i++) {
+      if(i != target) {
+         document.querySelectorAll('.faq-list li')[i].style = "max-height: 60px;";
+         document.querySelectorAll('.faq-list li div svg')[target].style = "transform: translateY(-100%) rotate(45deg);";
+         openState[i] = false;
+      }
+   }
+   //이벤트 발생 요소에 대한 실행 처리
+   if(openState[target]) {
+      document.querySelectorAll('.faq-list li')[target].style = "max-height: 69px;";
+      document.querySelectorAll('.faq-list li div svg')[target].style = "transform: translateY(-100%) rotate(45deg);";
+      openState[target] = false;
+   }
+   else {
+      document.querySelectorAll('.faq-list li')[target].style = "max-height: 500px;";
+      document.querySelectorAll('.faq-list li div svg')[target].style = "transform: translateY(-100%) rotate(90deg);";
+      openState[target] = true;
+   }
+}
 
