9f4f3c2986777914cc31eff3b62d7c112b4c3898
dohee93 <rlaehgml03@gmail.com>
2021-04-01 17:01:49 +0900

    update acoordion js

--- js/index.js
+++ js/index.js
@@ -21,30 +21,37 @@ var changeHero = setInterval(changeBackground, 3000);
 
 
 
-// Accordion Toggle JS
-//var openState = [false,false,false,false,false,false];
-var openState = new Boolean(false);
-   
-   
-function contentOpen(target) {
+// Accordion Toggle JS   
 
-   for(i = 0; i < 6; i++) {
-      if(i != target) {
-         document.querySelectorAll('.faq-list li')[i].style = "max-height: 60px;";
-         document.querySelectorAll('.faq-list li div svg')[target].style = "transform: translateY(-100%) rotate(45deg);";
-         openState[i] = false;
+var openState = new Boolean(false);
+var faq = document.getElementsByClassName('faq-item');
+var accordSymbol = document.querySelectorAll('.faq-list li div svg');
+   var faqList = document.querySelectorAll('.faq-list li');
+
+function contentOpen(target) {
+   faq[target].addEventListener('click', function(){    
+      for(var i = 0; i < 6; i++) {
+         if(i != target) {
+            faqList[i].style = "max-height: 69px;";
+            accordSymbol[i].style = "transform: translateY(-100%) rotate(45deg);";
+            openState[i] = false;
+         }
       }
-   }
-   //이벤트 발생 요소에 대한 실행 처리
-   if(openState[target]) {
-      document.querySelectorAll('.faq-list li')[target].style = "max-height: 69px;";
-      document.querySelectorAll('.faq-list li div svg')[target].style = "transform: translateY(-100%) rotate(45deg);";
-      openState[target] = false;
-   }
-   else {
-      document.querySelectorAll('.faq-list li')[target].style = "max-height: 500px;";
-      document.querySelectorAll('.faq-list li div svg')[target].style = "transform: translateY(-100%) rotate(90deg);";
-      openState[target] = true;
-   }
+      //이벤트 발생 요소에 대한 실행 처리
+      if(openState[target]) {
+         faqList[target].style = "max-height: 69px;";
+         accordSymbol[target].style = "transform: translateY(-100%) rotate(45deg);";
+         openState[target] = false;
+      }
+      else {
+         faqList[target].style = "max-height: 500px;";
+         accordSymbol[target].style = "transform: translateY(-100%) rotate(90deg);";
+         openState[target] = true;
+      }
+   });
+}
+
+for(var i = 0; i < faq.length; i++ ){
+   contentOpen(i);
 }
 
